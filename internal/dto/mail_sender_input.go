32d4ea6587bafdf4843f764f98e3aa89b11b993c
Jeanluca <jeanlucafp@gmail.com>
2025-03-27 17:24:52 -0300

    add payment email template and refactor MailSenderInputDto structure

--- internal/dto/mail_sender_input.go
+++ internal/dto/mail_sender_input.go
@@ -6,18 +6,25 @@ import (
 	"github.com/Jean1dev/communication-service/internal/templates"
 )
 
+type CustomBodyPropsDto struct {
+	Username    string  `json:"username"`
+	Operation   string  `json:"operation"`
+	Amount      float32 `json:"amount"`
+	TotalAmount float32 `json:"totalAmount"`
+
+	ChavePix      string `json:"chavePix"`
+	PixCopiaECola string `json:"pixCopiaECola"`
+	PixQRCode     string `json:"pixQRCode"`
+	LinkPagamento string `json:"linkPagamento"`
+}
+
 type MailSenderInputDto struct {
-	Subject         string `json:"subject"`
-	Body            string `json:"message"`
-	Recipient       string `json:"to"`
-	TemplateCode    int    `json:"templateCode"`
-	AttachmentLink  string `json:"attachmentLink"`
-	CustomBodyProps struct {
-		Username    string  `json:"username"`
-		Operation   string  `json:"operation"`
-		Amount      float32 `json:"amount"`
-		TotalAmount float32 `json:"totalAmount"`
-	} `json:"customBodyProps"`
+	Subject         string             `json:"subject"`
+	Body            string             `json:"message"`
+	Recipient       string             `json:"to"`
+	TemplateCode    int                `json:"templateCode"`
+	AttachmentLink  string             `json:"attachmentLink"`
+	CustomBodyProps CustomBodyPropsDto `json:"customBodyProps"`
 }
 
 func (m *MailSenderInputDto) Validate() error {
@@ -39,6 +46,16 @@ func (m *MailSenderInputDto) GetTemplate() string {
 			m.CustomBodyProps.Operation,
 			m.CustomBodyProps.Amount,
 			m.CustomBodyProps.TotalAmount,
+		)
+	}
+
+	if m.TemplateCode == 2 {
+		return templates.PaymentTemplate(
+			m.CustomBodyProps.LinkPagamento,
+			m.CustomBodyProps.ChavePix,
+			m.CustomBodyProps.PixCopiaECola,
+			m.CustomBodyProps.Username,
+			m.Body,
 		)
 	}
 	return templates.Default(m.Subject, m.Body)
--- internal/templates/templates_mail.go
+++ internal/templates/templates_mail.go
@@ -7,6 +7,134 @@ import (
 	"time"
 )
 
+func PaymentTemplate(paymentLink, pixKey, pixCopiaECola, username, bodyMessage string) string {
+	htmlTemplate := `<!DOCTYPE html>
+<html style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+<head>
+<meta name="viewport" content="width=device-width" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Payment Details</title>
+
+<style type="text/css">
+img {
+max-width: 100%;
+}
+body {
+-webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6em;
+}
+body {
+background-color: #f6f6f6;
+}
+@media only screen and (max-width: 640px) {
+  body {
+	padding: 0 !important;
+  }
+  h1 {
+	font-weight: 800 !important; margin: 20px 0 5px !important;
+  }
+  h2 {
+	font-weight: 800 !important; margin: 20px 0 5px !important;
+  }
+  h3 {
+	font-weight: 800 !important; margin: 20px 0 5px !important;
+  }
+  h4 {
+	font-weight: 800 !important; margin: 20px 0 5px !important;
+  }
+  h1 {
+	font-size: 22px !important;
+  }
+  h2 {
+	font-size: 18px !important;
+  }
+  h3 {
+	font-size: 16px !important;
+  }
+  .container {
+	padding: 0 !important; width: 100% !important;
+  }
+  .content {
+	padding: 0 !important;
+  }
+  .content-wrap {
+	padding: 10px !important;
+  }
+  .invoice {
+	width: 100% !important;
+  }
+}
+</style>
+</head>
+
+<body itemscope itemtype="http://schema.org/EmailMessage" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6em; background-color: #f6f6f6; margin: 0;" bgcolor="#f6f6f6">
+
+<table class="body-wrap" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; background-color: #f6f6f6; margin: 0;" bgcolor="#f6f6f6"><tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0;" valign="top"></td>
+	<td class="container" width="600" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; display: block !important; max-width: 600px !important; clear: both !important; margin: 0 auto;" valign="top">
+	  <div class="content" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; max-width: 600px; display: block; margin: 0 auto; padding: 20px;">
+		<table class="main" width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; border-radius: 3px; background-color: #fff; margin: 0; border: 1px solid #e9e9e9;" bgcolor="#fff"><tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td class="content-wrap" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 20px;" valign="top">
+			  <table width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+				<tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+				  <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+					<h2 style="font-family: 'Helvetica Neue',Helvetica,Arial,'Lucida Grande',sans-serif; box-sizing: border-box; font-size: 24px; color: #000; line-height: 1.2em; font-weight: 400; margin: 0;">Detalhes do Pagamento</h2>
+				  </td>
+				</tr>
+				<tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+				  <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+					<strong>Link de Pagamento:</strong> <a href="{{.PaymentLink}}" style="color: #348eda; text-decoration: underline;">Clique aqui</a>
+				  </td>
+				</tr>
+				<tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+				  <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+					<strong>Chave Pix:</strong> {{.PixKey}}
+				  </td>
+				</tr>
+				<tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+				  <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+					<strong>Pix Copia e Cola:</strong> {{.PixCopiaECola}}
+				  </td>
+				</tr>
+				<tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+				  <td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+					<p>{{.BodyMessage}}</p>
+				  </td>
+				</tr>
+			  </table>
+			</td>
+		  </tr></table>
+	  </div>
+	</td>
+	<td style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0;" valign="top"></td>
+  </tr></table>
+</body>
+</html>
+	`
+
+	data := struct {
+		PaymentLink   string
+		PixKey        string
+		PixCopiaECola string
+		BodyMessage   string
+	}{
+		PaymentLink:   paymentLink,
+		PixKey:        pixKey,
+		PixCopiaECola: pixCopiaECola,
+		BodyMessage:   bodyMessage,
+	}
+
+	tmpl, err := template.New("paymentTemplate").Parse(htmlTemplate)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	var result strings.Builder
+	err = tmpl.Execute(&result, data)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.String()
+}
+
 func CaixinhaTemplate(username, op string, amout, totalAmount float32) string {
 	htmlTemplate := `<!DOCTYPE html>
 <html  style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
@@ -210,7 +338,7 @@ func Default(subject string, message string) string {
 											this email was sending by JEANLUCAFP Consultoria de TI LTDA.
 										</td>
 									</tr><tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
-											<a href="https://github.com/jean1dev" class="btn-primary" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; color: #FFF; text-decoration: none; line-height: 2em; font-weight: bold; text-align: center; cursor: pointer; display: inline-block; border-radius: 5px; text-transform: capitalize; background-color: #348eda; margin: 0; border-color: #348eda; border-style: solid; border-width: 10px 20px;">Fale comigo</a>
+											<a href="https://calendly.com/jeanlucafp/consultoria" class="btn-primary" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; color: #FFF; text-decoration: none; line-height: 2em; font-weight: bold; text-align: center; cursor: pointer; display: inline-block; border-radius: 5px; text-transform: capitalize; background-color: #348eda; margin: 0; border-color: #348eda; border-style: solid; border-width: 10px 20px;">Fale comigo</a>
 										</td>
 									</tr><tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td class="content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
 											Obrigado.
@@ -225,7 +353,6 @@ func Default(subject string, message string) string {
 	</html>
 	`
 
-	// Create a data structure to pass the values to the template
 	data := struct {
 		Subject string
 		Message string
@@ -234,19 +361,16 @@ func Default(subject string, message string) string {
 		Message: message,
 	}
 
-	// Create a new template and parse the HTML
 	tmpl, err := template.New("emailTemplate").Parse(htmlTemplate)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	// Execute the template with the provided data
 	var result strings.Builder
 	err = tmpl.Execute(&result, data)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	// Return the generated HTML content as a string
 	return result.String()
 }
